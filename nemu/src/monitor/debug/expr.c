193bf2b8e10b84f683fe6e0c608f1e4266160238
tracer-ics2017 <tracer@njuics.org>
2023-03-18 16:33:21 +0800

    >  compile 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:33:21 up  1:35,  1 user,  load average: 0.22, 0.20, 0.18 b69c8ed6b3f85845e7651f5145a99799e3d92ac

--- nemu/src/monitor/debug/expr.c
+++ nemu/src/monitor/debug/expr.c
@@ -94,7 +94,7 @@ static bool make_token(char *e) {
             *(tokens[nr_token].str+substr_len)='\0';
             printf("\n%s",tokens[nr_token].str);
             printf("\n%d",substr_len);
-            printf("\n%d",*(tokens[nr_token].str+substr_len));
+            printf("\n%d",*(tokens[nr_token].str+substr_len+1));
             break;
           //default: TODO();
         }
