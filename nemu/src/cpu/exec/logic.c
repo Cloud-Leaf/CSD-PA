31788fd4b532df4d0f30d6c1843e285f4ee6623b
tracer-ics2017 <tracer@njuics.org>
2023-05-05 19:35:57 +0800

    >  compile 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  19:35:57 up  5:58,  1 user,  load average: 0.77, 0.32, 0.26 a5e558c8a5e0a1abc7b0cc96114d7d2a4e283589

--- nemu/src/cpu/exec/logic.c
+++ nemu/src/cpu/exec/logic.c
@@ -4,6 +4,7 @@ make_EHelper(test) {
   //TODO();
 
   rtl_and(&t2,&id_dest->val,&id_src->val);
+  printf("%u",t2);
   rtl_update_ZFSF(&t2,id_dest->width);
   rtl_set_CF(&tzero);
   rtl_set_OF(&tzero);
