c8b1e2cd0cf9347a7ffc56777737e9c30d9f9b80
tracer-ics2017 <tracer@njuics.org>
2023-05-07 21:04:06 +0800

    >  run 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:04:06 up 45 min,  1 user,  load average: 0.23, 0.48, 0.38 f388830168f2a79e80dbbc4a2e3b5dfed1b63640

--- navy-apps/libs/libos/src/nanos.c
+++ navy-apps/libs/libos/src/nanos.c
@@ -31,7 +31,7 @@ int _write(int fd, void *buf, size_t count){
 }
 
 void *_sbrk(intptr_t increment){
-  extern end;
+  extern char end;
   static intptr_t probreak=(intptr_t)&end;
 
   intptr_t probreak_new=probreak+increment;
