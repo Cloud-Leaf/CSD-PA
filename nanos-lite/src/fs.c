755b4beae0c65e125202286d8a5b89af350c0054
tracer-ics2017 <tracer@njuics.org>
2023-05-07 21:54:36 +0800

    >  run 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:54:36 up  1:35,  1 user,  load average: 0.12, 0.23, 0.33 ce8528a5ef429c4090ed492182d97f0faa4e44a5

--- nanos-lite/src/fs.c
+++ nanos-lite/src/fs.c
@@ -4,6 +4,7 @@ typedef struct {
   char *name;
   size_t size;
   off_t disk_offset;
+  off_t open_offset;
 } Finfo;
 
 enum {FD_STDIN, FD_STDOUT, FD_STDERR, FD_FB, FD_EVENTS, FD_DISPINFO, FD_NORMAL};
@@ -24,3 +25,60 @@ static Finfo file_table[] __attribute__((used)) = {
 void init_fs() {
   // TODO: initialize the size of /dev/fb
 }
+
+size_t fs_filesz(int fd) {
+  assert(fd>=0 && fd<NR_FILES);//不能超过给定文件个数
+  return file_table[fd].size;
+}
+
+off_t disk_offset(int fd) {
+  assert(fd>=0 && fd<NR_FILES);//不能超过给定文件个数
+  return file_table[fd].disk_offset;
+}
+
+off_t get_open_offset(int fd) {
+  assert(fd>=0 && fd<NR_FILES);//不能超过给定文件个数
+  return file_table[fd].open_offset;
+}
+
+//设置open_offset，每次从指定位置读取
+void set_open_offset(int fd,off_t n) {
+  assert(fd>=0 && fd<NR_FILES);//不能超过给定文件个数
+  assert(n>=0);
+  if(n>file_table[fd].size) 
+    n=file_table[fd].size;
+  file_table[fd].open_offset=n;
+}
+
+extern void ramdisk_read(void *buf,off_t offset,size_t len);
+extern void ramdisk_write(void *buf,off_t offset,size_t len);
+
+int fs_open(const char* filename,int flags,int mode) {
+  for(int i=0;i<NR_FILES;i++) 
+    if(strcmp(filename,file_table[i].name)==0) {
+      return i;
+    }
+  
+  panic("This file is not exist!");
+  return -1;
+}
+
+ssize_t fs_read(int fd,void* buf,size_t len) {
+  assert(fd>=0 && fd<NR_FILES);//不能超过给定文件个数
+  
+  if(fd<3) {
+    panic("arg invalid:fd<3");
+    return 0;
+  }
+  int n=fs_filesz(fd)-get_open_offset(fd);//最多能读取多少字节
+  if(n>len) 
+    n=len;
+  ramdisk_read(buf,disk_offset(fd)+get_open_offset(fd),n);
+  set_open_offset(fd,get_open_offset(fd)+n);//设置偏移量
+  return n;
+}
+
+int fs_close(int fd) {
+  assert(fd>=0 && fd<NR_FILES);//不能超过给定文件个数
+  return 0;
+} 
