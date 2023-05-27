2a1fca75b293245ace25c16fb034330c1f0d8b3e
tracer-ics2017 <tracer@njuics.org>
2023-05-27 21:32:41 +0800

    >  compile 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  21:32:41 up  3:26,  1 user,  load average: 0.24, 0.25, 0.27 57e9d58d5eff8bce1ea86c3531265a76db7634f2

--- nemu/src/memory/memory.c
+++ nemu/src/memory/memory.c
@@ -39,11 +39,24 @@ void paddr_write(paddr_t addr, int len, uint32_t data) {
 uint32_t vaddr_read(vaddr_t addr, int len) {
   //return paddr_read(addr, len);
   if(PTE_ADDR(addr)!=PTE_ADDR(addr+len-1)){
-    printf("error:data pass two pages:addr=0x%x,len=%d!\n",addr,len);
-    
+    //printf("error:data pass two pages:addr=0x%x,len=%d!\n",addr,len);
     //paddr_t paddr=page_translate(addr,false);
     //paddr_read(paddr,len);
-    assert(0);
+    //assert(0);
+
+    int num1=0x1000-OFF(addr);
+    int num2=len-num1;
+    paddr_t paddr1=page_translate(addr ,false);
+    paddr_t paddr2=page_translate(addr +num1,false);
+
+    uint32_t low=paddr_read(paddr1,num1);
+    uint32_t high=paddr_read(paddr2,num2);
+    //printf( "low=Ox%x,high=Ox%x\n",low,high);
+
+    uint32_t result=high<<(num1*8)|low;
+    //printf( "result=O×%x\n",result);
+    return result;
+
   }
   else {
     paddr_t paddr=page_translate(addr,false);
@@ -56,12 +69,27 @@ uint32_t vaddr_read(vaddr_t addr, int len) {
 void vaddr_write(vaddr_t addr, int len, uint32_t data) {
   //paddr_write(addr, len, data);
   if(PTE_ADDR(addr)!=PTE_ADDR(addr+len-1)){
-    printf("error:data pass two pages:addr=0x%x,len=%d!\n",addr,len);
-    assert(0);
+    //printf("error:data pass two pages:addr=0x%x,len=%d!\n",addr,len);
+    //assert(0);
+
+    int num1=0x1000-OFF(addr);
+    int num2=len-num1;
+    paddr_t paddr1=page_translate(addr ,false);
+    paddr_t paddr2=page_translate(addr +num1,false);
+
+    uint32_t low=data&(~0u >> ((4-num1)<< 3));
+    uint32_t high=data>>((4 - num2)<< 3);
+
+    //printf( "low=Ox%x,high=Ox%x\n",low,high);
+    paddr_write(paddr1,num1,low);
+    paddr_write(paddr2,num2,high);
+    
+    return;
+
   }
   else {
     paddr_t paddr=page_translate(addr,true);
-    return paddr_write(paddr,len,data);
+    paddr_write(paddr,len,data);
   }
 }
 
