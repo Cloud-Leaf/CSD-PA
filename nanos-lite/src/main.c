6d55cd607b8b24d39665a895357111837bc5ccae
tracer-ics2017 <tracer@njuics.org>
2023-05-28 16:53:09 +0800

    >  run 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  16:53:09 up 9 min,  1 user,  load average: 0.35, 0.22, 0.12 e252820aeafcfd7b48d6c33047b889c976cbf2c1

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,8 +30,11 @@ int main() {
 
   init_fs();
 
-  uint32_t entry = loader(NULL, "/bin/pal");
-  ((void (*)(void))entry)();
+  //uint32_t entry = loader(NULL, "/bin/dummy");
+  //((void (*)(void))entry)();
+
+  extern void load_prog(const char* filename);
+  load_prog("/bin/dummy");
 
   panic("Should not reach here");
 }
