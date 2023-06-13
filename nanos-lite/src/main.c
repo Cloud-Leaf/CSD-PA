5c851d47c1ad38395ff389db61031434250bb38b
tracer-ics2017 <tracer@njuics.org>
2023-06-13 23:55:53 +0800

    >  run 2012116 cloud Linux ubuntu 4.15.0-142-generic #146~16.04.1-Ubuntu SMP Tue Apr 13 09:26:57 UTC 2021 i686 i686 i686 GNU/Linux  23:55:53 up  1:54,  1 user,  load average: 0.96, 0.67, 0.43 ba742d58b65973d3884c652787bcace316681bad

--- nanos-lite/src/main.c
+++ nanos-lite/src/main.c
@@ -30,11 +30,11 @@ int main() {
 
   init_fs();
 
-  //uint32_t entry = loader(NULL, "/bin/dummy");
-  //((void (*)(void))entry)();
+  uint32_t entry = loader(NULL, "/bin/pal");
+  ((void (*)(void))entry)();
 
   extern void load_prog(const char* filename);
-  load_prog("/bin/pal");
+  //load_prog("/bin/pal");
   //load_prog("/bin/hello");
 
   _trap();
